ffa6e35f7461d5dc9f29a98f2d681316417d8321
irene <irenejim9504@gmail.com>
2024-02-29 08:41:51 +0300

    Added support for printing an empty list when length is 0 in print_fibonacci() function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length < 0:
+        print("Length should be a non-negative integer")
+        return
+    elif length == 0:
+        fib_sequence = []
+    elif length == 1:
+        fib_sequence = [0]
+    elif length == 2:
+        fib_sequence = [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < length:
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    print(fib_sequence)
+
+
